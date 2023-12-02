81df52030d802edf16748740c009721ced3678d6
Elliott Morgan <awesomescar16@outlook.com>
2023-12-02 16:57:30 +0000

    Day 2 completed

---
+++ day2/day2p1.py
@@ -0,0 +1,26 @@
+document = open("day2/day2.txt").read().splitlines()
+maximum = {
+    "r":12,
+    "g":13,
+    "b":14
+}
+total = 0
+for line in document:
+    possible = True
+    for character_index in range(8, len(line)):
+        if line[character_index].isnumeric():
+            num = int(line[character_index])
+            if line[character_index + 1].isnumeric():
+                num = num * 10 + int(line[character_index + 1])
+                character_index += 1
+            colour = line[character_index + 2]
+            if num > maximum[colour]:
+                possible = False
+    if possible:
+        num = int(line[5])
+        if line[6].isnumeric():
+            num = num * 10 + int(line[6])
+            if line[7].isnumeric():
+                num = num * 10 + int(line[7])
+        total += num
+print (total)
---
+++ day2/day2p2.py
@@ -0,0 +1,24 @@
+document = open("day2/day2.txt").read().splitlines()
+maximum = {
+    "r":0,
+    "g":0,
+    "b":0
+}
+total = 0
+for line in document:
+    maximum = {
+    "r":0,
+    "g":0,
+    "b":0}
+    for character_index in range(8, len(line)):
+        if line[character_index].isnumeric():
+            num = int(line[character_index])
+            if line[character_index + 1].isnumeric():
+                num = num * 10 + int(line[character_index + 1])
+                character_index += 1
+            colour = line[character_index + 2]
+            if num > maximum[colour]:
+                maximum[colour] = num
+    power = maximum["r"] * maximum["g"] * maximum["b"]
+    total += power
+print(total)
